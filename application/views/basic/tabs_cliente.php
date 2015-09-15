1af63baab102d8468ef9265cd976a8b08b71be11
neftona <isc_nefgarcia@hotmail.com>
2015-09-15 11:10:23 -0500

    menu

--- application/views/basic/tabs_cliente.php
+++ application/views/basic/tabs_cliente.php
@@ -2,14 +2,14 @@
 	<a href="<?= site_url('clientes/ver/'.$datos_cliente->id) ?>" Style="float:left; margin-right:100px; padding-top:10px"><?= $datos_cliente->nombre ?></a>
 	<ul class="nav nav-pills">
 		<?php $activo=""; if($tab == 'detalles'){$activo="active";} ?>
-		<li class="<?= $activo ?>"><a href="<?= site_url('clientes/ver/'.$datos_cliente->id) ?>">Detalles</a></li>
+		<li class="<?= $activo ?>"><a href="<?= site_url('clientes/ver/'.id_cliente) ?>">Detalles</a></li>
 		<?php $activo=""; if($tab == 'proyec_tarea'){$activo="active";} ?>
-		<li class="<?= $activo ?>"><a href="<?= site_url('proyectos/cliente/'.$datos_cliente->id) ?>">Proyectos</a></li>
+		<li class="<?= $activo ?>"><a href="<?= site_url('proyectos/cliente/'.id_cliente) ?>">Proyectos</a></li>
 		<?php $activo=""; if($tab == 'tarea_vista'){$activo="active";} ?>
-		<li class="<?= $activo ?>"><a href="<?= site_url('tareas/cliente/'.$datos_cliente->id) ?>">Tareas</a></li>
+		<li class="<?= $activo ?>"><a href="<?= site_url('tareas/cliente/'.id_cliente) ?>">Tareas</a></li>
 		<?php $activo=""; if($tab == 'contacto'){$activo="active";} ?>
-		<li class="<?= $activo ?>"><a href="<?= site_url('clientes/contacto/'.$datos_cliente->id) ?>">Contactos</a></li>
+		<li class="<?= $activo ?>"><a href="<?= site_url('clientes/contacto/'.id_cliente) ?>">Contactos</a></li>
 		<?php $activo=""; if($tab == 'bitacora'){$activo="active";} ?>
-		<li class="<?= $activo ?>"><a href="<?= site_url('bitacora/cliente/'.$datos_cliente->id) ?>">Bitacora</a></li>
+		<li class="<?= $activo ?>"><a href="<?= site_url('bitacora/cliente/'.id_cliente) ?>">Bitacora</a></li>
 	</ul>
 </ul> 
